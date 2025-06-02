134b40d29429bc0178bea263a087231e917fe107
Ratan Biswakarmakar <hackerrahul819@gmail.com>
2025-06-02 19:58:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,21 +18,21 @@ from automl_backend import add_automl_routes
 from data_profiling_backend import add_data_profiling_routes
 from llm_code_generation_backend import add_llm_code_generation_routes
 from llm_mesh_backend import add_llm_mesh_routes
-from automated_feature_engineering_backend import add_feature_engineering_routes
-from ai_copilot_backend import add_ai_copilot_routes
+#from automated_feature_engineering_backend import add_feature_engineering_routes
+#from ai_copilot_backend import add_ai_copilot_routes
 from genai_docs_backend import add_genai_docs_routes
-from data_drift_detection_backend import add_data_drift_detection_routes
+#from data_drift_detection_backend import add_data_drift_detection_routes
 from model_training_deployment_backend import add_model_training_deployment_routes
-from embedded_ml_clustering_backend import add_embedded_ml_clustering_routes
+#from embedded_ml_clustering_backend import add_embedded_ml_clustering_routes
 from time_series_forecasting_backend import add_time_series_forecasting_routes
-from ai_driven_cataloging_backend import add_ai_driven_cataloging_routes
+#from ai_driven_cataloging_backend import add_ai_driven_cataloging_routes
 from model_governance_backend import add_model_governance_routes
 from code_free_modeling_backend import add_code_free_modeling_routes
 from prompt_engineering_interface import add_prompt_engineering_routes
-from integration_notebooks_backend import add_integration_notebooks_routes
+#from integration_notebooks_backend import add_integration_notebooks_routes
 from ai_data_enrichment_backend import add_ai_data_enrichment_routes
 from deploy_to_alteryx_promote import add_alteryx_promote_routes
-from out_of_box_ml_backend import add_out_of_box_ml_routes
+#from out_of_box_ml_backend import add_out_of_box_ml_routes
 from clustering_backend import add_clustering_routes
 from object_detection import object_detection_bp
 from text_classification_summarization_backend import add_text_classification_routes
@@ -77,21 +77,21 @@ add_automl_routes(app, data_store, client)
 add_data_profiling_routes(app, data_store, client)
 add_llm_code_generation_routes(app, data_store, client)
 add_llm_mesh_routes(app, data_store, client)
-add_feature_engineering_routes(app, data_store, client)
-add_ai_copilot_routes(app, data_store, client)
+#add_feature_engineering_routes(app, data_store, client)
+#add_ai_copilot_routes(app, data_store, client)
 add_genai_docs_routes(app, data_store, client)
-add_data_drift_detection_routes(app, data_store, client)
+#add_data_drift_detection_routes(app, data_store, client)
 add_model_training_deployment_routes(app, data_store, client)
-add_embedded_ml_clustering_routes(app, data_store, client)
+#add_embedded_ml_clustering_routes(app, data_store, client)
 add_time_series_forecasting_routes(app, data_store, client)
-add_ai_driven_cataloging_routes(app, data_store, client)
+#add_ai_driven_cataloging_routes(app, data_store, client)
 add_model_governance_routes(app, data_store, client)
 add_code_free_modeling_routes(app, data_store, client)
 add_prompt_engineering_routes(app, data_store, client)
-add_integration_notebooks_routes(app, data_store, client)
+#add_integration_notebooks_routes(app, data_store, client)
 add_ai_data_enrichment_routes(app, data_store, client)
 add_alteryx_promote_routes(app, data_store, client)
-add_out_of_box_ml_routes(app, data_store, client)
+#add_out_of_box_ml_routes(app, data_store, client)
 add_clustering_routes(app, data_store, client)
 app.register_blueprint(object_detection_bp)
 add_text_classification_routes(app, data_store, client)
